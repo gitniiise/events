b7d82f02516fa72f69aef8fd87275494d32ade27
denise.bebenroth <denise.bebenroth@chefkoch.de>
2024-04-16 18:15:41 +0200

    setup event project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# real-world-vue
+# events: real-world-vue
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import EventService from "@/services/EventService.js";
+
+const props = defineProps({
+  id: {
+    required: true,
+  }
+})
+
+const event = ref(null)
+
+onMounted(() => {
+  EventService.getEvent(props.id)
+      .then((response) => {
+        event.value = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+})
+</script>
+
+<template>
+  <div v-if="event">
+    <div>
+      <h1>{{ event.title }}</h1>
+      <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+      <p>{{ event.description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
